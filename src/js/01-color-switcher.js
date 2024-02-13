9a36ae37a512f9c4a350be2f6050cc817d84eaaa
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-02-13 11:46:20 +0100

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
 
+
+btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }, 1000);
+    btnStart.disabled = true;
+});
+
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+})
