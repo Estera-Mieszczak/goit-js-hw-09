4670934b32909283970ea41a9a986cb87b3d47c7
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-02-18 20:01:41 +0100

    Update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,52 @@
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector(".form");
 
+const amount = Number(form.elements.amount.value);
+const delay = Number(form.elements.delay.value);
+const step = Number(form.elements.step.value);
 
-console.log(form);
-
-  
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resovle({ position, delay });
+        resolve({ position, delay });
       } else {
-        reject('error');
+        reject({ position, delay });
       }
-    });
+    }, delay);
   })}
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault;
-  let position = 0;
-  let amount = event.currentTarget.form[2].value;
-  let delay = event.currentTarget.form[0].value;
-  
-  timerId = setInterval(() => {
-    if (position < amount) {
-      createPromise(position, delay)
-        .then(value =>
-          alert(` Fulfilled promise ${position} in ${delay}ms`)
-        )
-        .catch(error =>
-          alert(`Rejected promise ${position} in ${delay}ms`)
-        );
-      position += 1;
-      delay += event.currentTarget.form[1].value;
-    }
-    else {
-      clearInterval(timerId);
-    }
-  }, delay);
-  
+  event.preventDefault();
+ 
+  for (let i = 0; i < amount; i += 1) {
+    
+    createPromise(i, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  form.reset();
 })
+
+// timerId = setInterval(() => {
+//     if (position < amount) {
+//       createPromise(position, delay)
+//         .then(value =>
+//           alert(` Fulfilled promise ${position} in ${delay}ms`)
+//         )
+//         .catch(error =>
+//           alert(`Rejected promise ${position} in ${delay}ms`)
+//         );
+//       position += 1;
+//       delay += event.currentTarget.form[1].value;
+//     }
+//     else {
+//       clearInterval(timerId);
+//     }
+//   }, delay);
