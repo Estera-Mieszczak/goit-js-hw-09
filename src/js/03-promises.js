5c49f20cdebd3f0ed785d90eff7a2563efa36e14
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-02-19 18:16:47 +0100

    Update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,9 +2,22 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector(".form");
 
-const amount = Number(form.elements.amount.value);
-const delay = Number(form.elements.delay.value);
-const step = Number(form.elements.step.value);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const amount = Number(event.target.elements.amount.value);
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  for (let i = 0; i < amount; i ++) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  form.reset();
+})
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -18,35 +31,4 @@ function createPromise(position, delay) {
     }, delay);
   })}
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
- 
-  for (let i = 0; i < amount; i += 1) {
-    
-    createPromise(i, delay + i * step)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-  }
-  form.reset();
-})
 
-// timerId = setInterval(() => {
-//     if (position < amount) {
-//       createPromise(position, delay)
-//         .then(value =>
-//           alert(` Fulfilled promise ${position} in ${delay}ms`)
-//         )
-//         .catch(error =>
-//           alert(`Rejected promise ${position} in ${delay}ms`)
-//         );
-//       position += 1;
-//       delay += event.currentTarget.form[1].value;
-//     }
-//     else {
-//       clearInterval(timerId);
-//     }
-//   }, delay);
